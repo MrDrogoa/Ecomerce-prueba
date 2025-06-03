580596a49e8be9a68bb7f3188ade82e91706eacb
daniel <picisdaniel2016@gmail.com>
2025-06-03 01:27:03 -0400

    avance con los detalles del producto y tambien agregue la libreria de bootstrap

--- details.php
+++ details.php
@@ -20,24 +20,39 @@ if ($id == "" || $token == "") {
     $sql->execute([$id]);
     if ($sql->fetchColumn() > 0) {
 
-      $sql = $con->prepare("SELECT nombre, descripcion, precio FROM productos WHERE id=? AND activo=1 LIMIT 1");
+      $sql = $con->prepare("SELECT nombre, descripcion, precio, descuento FROM productos WHERE id=? AND activo=1 LIMIT 1");
       $sql->execute([$id]);
       $row = $sql->fetch(PDO::FETCH_ASSOC);
       $nombre = $row["nombre"];
       $descripcion = $row["descripcion"];
       $precio = $row["precio"];
+      // descuento
+      $descuento = $row["descuento"];
+      $precio_desc = $precio - (($precio * $descuento) / 100);
+      // traemos la imagen de forma dinamica
+      $dir_images = "img/producto/" . $id . "/";
+
+      // imagen principal 
+      $rutaImg = $dir_images . "image.png";
+      if (!file_exists($rutaImg)) {
+        $rutaImg = "img/no-photo.jpg";
+      }
+
+      $imagenes = array();
+      $dir = dir($dir_images);
+
+      // con esto traemos las imagenes
+      while (($archivo = $dir->read()) != false) {
+        if ($archivo != "image.png" && (strpos($archivo, "png") || strpos($archivo, "jpg"))) {
+          $imagenes[] = $dir_images . $archivo;
+        }
+      }
     }
   } else {
     echo "error al procesar la peticion";
     exit;
   }
 }
-
-$sql = $con->prepare("SELECT id, nombre, descripcion, precio FROM productos WHERE activo=1");
-$sql->execute();
-// con esto llama a los productos de toda la tabla PDO::FETCH_ASSOC con esto
-// estamos diciendo que me lo traiga por nombre de columnas
-$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -48,7 +63,10 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>E-commerce Website</title>
+  <!-- tailwind -->
   <link href="./output.css" rel="stylesheet" />
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 
 <body class="bg-gray-100">
@@ -71,17 +89,46 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
   </header>
 
   <main class="container mx-auto px-4 py-8">
-    <div class="grid grid-cols-1 md:grid-cols-2">
-      <div class="">
-        <img src="./img/producto/1/image.png" alt="image" class="w-full max-w-md object-cover">
+    <div class="flex justify-center flex-col md:flex-row">
+      <div class="w-full flex justify-center">
+
+        <div id="carouselImages" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="<?= $rutaImg ?>" alt="image" class="w-full md:w-3/4">
+            </div>
+            <?php foreach ($imagenes as $img) { ?>
+              <div class="carousel-item">
+                <img src="<?= $img ?>" alt="image" class="w-full md:w-3/4">
+              </div>
+            <?php } ?>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselImages" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselImages" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+
       </div>
-      <div class="">
+      <div class="w-full">
         <h2 class=""><?= $nombre ?></h2>
-        <h2><?= MONEDA . number_format($precio, 2, ".", ",") ?></h2>
+        <?php if ($descuento > 0) { ?>
+          <p><del><?= MONEDA . number_format($precio, 2, ".", ",") ?></del></p>
+          <h2>
+            <?= MONEDA . number_format($precio_desc, 2, ".", ",") ?>
+            <small class="text-success"><?= $descuento; ?>% descuento</small>
+          </h2>
+        <?php } else { ?>
+          <h2><?= MONEDA . number_format($precio, 2, ".", ",") ?></h2>
+        <?php } ?>
         <p><?= $descripcion ?></p>
-        <div class="grid grid-cols-5 gap-7">
+        <div class="flex justify-center md:justify-start">
           <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" type="button">Comprar ahora</button>
-          <button class="bg-white hover:bg-gray-600 px-4 py-2 rounded" type="button">Agregar al carrito ahora</button>
+          <button class="bg-white hover:bg-slate-300 px-4 py-2 rounded" type="button">Agregar al carrito ahora</button>
         </div>
       </div>
     </div>
@@ -122,6 +169,8 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
       </div>
     </footer> -->
 
+  <!-- bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 
 </body>
 
